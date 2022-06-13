41a33c48a425452b147c975142c910449954c43b
Muhammed Balta <muhammed.balta@hotmail.com>
2022-06-14 00:38:16 +0300

    DBContext and Postgre connection

---
+++ Contact.API/ContactAPIContext.cs
@@ -0,0 +1,12 @@
+﻿using Contact.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contact.API
+{
+    public class ContactAPIContext : DbContext
+    {
+        public ContactAPIContext(DbContextOptions<ContactAPIContext> options) : base(options){}
+        public DbSet<Person> People { get; set;}
+
+    }
+}
