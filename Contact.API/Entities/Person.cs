cc5c1b47f11fd2a6ba278836eadedcf436f1a47b
Muhammed Balta <muhammed.balta@hotmail.com>
2022-06-14 00:39:16 +0300

    Person entity and migration

---
+++ Contact.API/Entities/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Contact.API.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Company { get; set; }
+    }
+}
